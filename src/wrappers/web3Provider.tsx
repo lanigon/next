cc208402f486515d67b5b6dc657451cef84483d4
刘程鑫 <756004757@qq.com>
2024-12-03 14:29:12 +0800

    world initiation

--- src/wrappers/web3Provider.tsx
+++ src/wrappers/web3Provider.tsx
@@ -4,22 +4,33 @@ import { WagmiProvider } from 'wagmi'
 import React from 'react'
 import { http, createConfig } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
+import { ReactNode, useEffect } from 'react'
+import { MiniKit } from '@worldcoin/minikit-js'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'; 
 
-const Web3Provider = ({ children }:React.PropsWithChildren) => {
+const Web3Provider = ({ children }:{children: ReactNode}) => {
   const key = process.env.NEXT_PUBLIC_RPC_KEY
-
+  const queryClient = new QueryClient()
   const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(`https://mainnet.infura.io/v3/${key}`),
-    [sepolia.id]: http(`https://sepolia.infura.io/v3/${key}`),
-  },
+    chains: [mainnet, sepolia],
+    transports: {
+      [mainnet.id]: http(`https://mainnet.infura.io/v3/${key}`),
+      [sepolia.id]: http(`https://sepolia.infura.io/v3/${key}`),
+    },
   })
+  const miniapp_id = process.env.NEXT_PUBLIC_MINIAPP_ID
+  useEffect(() => {
+		// Passing appId in the install is optional 
+		// but allows you to access it later via `window.MiniKit.appId`
+		MiniKit.install(miniapp_id) 
+	}, [miniapp_id])
 
   return (
-    <WagmiProvider config={config}>
-      {children}
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        {children}
+      </WagmiProvider>
+    </QueryClientProvider>
   )
 }
 
